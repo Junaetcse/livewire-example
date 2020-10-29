5d618a2c62218b9eab645f01753416ab4b2b3550
DESKTOP-HDASJFM\junae <junaetcse@gmail.com>
2020-10-29 23:11:16 +0600

    add validation

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -3,9 +3,31 @@
 namespace App\Http\Livewire\Auth;
 
 use Livewire\Component;
+use App\User;
+use Hash;
 
 class Register extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $confirm_password = '';
+
+    public function register()
+    {
+
+        $data = $this->validate([
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:3|same:confirm_password'
+        ]);
+
+        User::create([
+            "email" => $data['email'],
+            "name" => 'Hellli',
+            "password" =>Hash::make($data['password'])
+        ]);
+
+        return \redirect('/');
+    }
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,5 +1,27 @@
 <div>
-    The best athlete wants his opponent at his best.
-
-    <h3>Hello</h3>
+    <form wire:submit.prevent="register">
+        <div>
+            <label for="email">Email</label>
+            <input wire:model="email" type="text" id="email" name="email">
+            @error('email')
+                <span>{{$message}}</span>
+                    @enderror
+                </div>
+            <div>
+            
+            <label for="password">Password</label>
+            <input wire:model="password" type="text" id="password" name="password">
+            @error('password')
+            <span>{{$message}}</span>
+                @enderror
+        </div>
+        <div>
+            
+            <label for="confirm_password">Confirm Password</label>
+            <input wire:model="confirm_password" type="text" id="confirm_password" name="confirm_password">
+        </div>
+        <div>
+            <input type="submit" value="Registration"> 
+        </div>
+    </form>
 </div>
